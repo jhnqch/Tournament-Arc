6bf62b77f2e16b38b0482d18aa6d4aee3e5f598f
John Quach <quachjohnhien@gmail.com>
2021-02-11 12:58:11 -0800

    Update drawerContent.js
    Implemented logout button in drawer navigation.

--- routes/drawerContent.js
+++ routes/drawerContent.js
@@ -1,159 +1,157 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
 import {
-    DrawerContentScrollView,
-    DrawerItem
-} from '@react-navigation/drawer';
-import {
-    Avatar,
-    Title,
-    Caption,
-    Paragraph,
-    Drawer,
-    Text,
-    TouchableRipple,
-    Switch
-} from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+  Avatar,
+  Title,
+  Caption,
+  Paragraph,
+  Drawer,
+  Text,
+  TouchableRipple,
+  Switch,
+} from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export function DrawerContent(props) {
-    return (
-        <View style={{ flex: 1 }}>
-            <DrawerContentScrollView {...props}>
-                <View style={styles.drawerContent}>
-                    <View style={styles.userInfoSection}>
-                        <View style={{ flexDirection: 'row', marginTop: 15 }}>
-                            <Avatar.Image
-                                size={50}
-                                source={require('../assets/bienfu.png')}
-                            />
-                            <View style={{
-                                marginLeft: 15,
-                                flexDirection: 'column'
-                            }}>
-                                <Title style={styles.title}>Guest User</Title>
-                                <Caption style={styles.caption}>@GuestUser</Caption>
-                            </View>
-                        </View>
-                    </View>
-                    <Drawer.Section style={styles.drawerSection}>
-                        <DrawerItem
-                            icon={({ color, size }) => (
-                                <Icon
-                                    name="home-outline"
-                                    color={color}
-                                    size={size}
-                                />
-                            )}
-                            label="Home"
-                            onPress={() => {props.navigation.navigate('Home')}}
-                        />
-                        <DrawerItem
-                            icon={({ color, size }) => (
-                                <Icon
-                                    name="account-outline"
-                                    color={color}
-                                    size={size}
-                                />
-                            )}
-                            label="Profile"
-                            onPress={() => { }}
-                        />
-                        <DrawerItem
-                            icon={({ color, size }) => (
-                                <Icon
-                                    name="gamepad"
-                                    color={color}
-                                    size={size}
-                                />
-                            )}
-                            label="My Tournaments"
-                            onPress={() => { }}
-                        />
-                        <DrawerItem
-                            icon={({ color, size }) => (
-                                <Icon
-                                    name="settings-outline"
-                                    color={color}
-                                    size={size}
-                                />
-                            )}
-                            label="Settings"
-                            onPress={() => { }}
-                        />
-                        <DrawerItem
-                            icon={({ color, size }) => (
-                                <Icon
-                                    name="account-check-outline"
-                                    color={color}
-                                    size={size}
-                                />
-                            )}
-                            label="About"
-                            onPress={() => {props.navigation.navigate('About')}}
-                        />
-                    </Drawer.Section>
-                </View>
-            </DrawerContentScrollView>
-            <Drawer.Section style={styles.bottomDrawerSection}>
-                <DrawerItem
-                    icon={({ color, size }) => (
-                        <Icon
-                            name="exit-to-app"
-                            color={color}
-                            size={size}
-                        />
-                    )}
-                    label="Log In"
-                    onPress={() => {props.navigation.navigate('Inputs')}}
-                />
-            </Drawer.Section>
+  return (
+    <View style={{ flex: 1 }}>
+      <DrawerContentScrollView {...props}>
+        <View style={styles.drawerContent}>
+          <View style={styles.userInfoSection}>
+            <View style={{ flexDirection: "row", marginTop: 15 }}>
+              <Avatar.Image
+                size={50}
+                source={require("../assets/bienfu.png")}
+              />
+              <View
+                style={{
+                  marginLeft: 15,
+                  flexDirection: "column",
+                }}
+              >
+                <Title style={styles.title}>Guest User</Title>
+                <Caption style={styles.caption}>@GuestUser</Caption>
+              </View>
+            </View>
+          </View>
+          <Drawer.Section style={styles.drawerSection}>
+            <DrawerItem
+              icon={({ color, size }) => (
+                <Icon name="home-outline" color={color} size={size} />
+              )}
+              label="Home"
+              onPress={() => {
+                props.navigation.navigate("Home");
+              }}
+            />
+            <DrawerItem
+              icon={({ color, size }) => (
+                <Icon name="account-outline" color={color} size={size} />
+              )}
+              label="Profile"
+              onPress={() => {}}
+            />
+            <DrawerItem
+              icon={({ color, size }) => (
+                <Icon name="gamepad" color={color} size={size} />
+              )}
+              label="My Tournaments"
+              onPress={() => {}}
+            />
+            <DrawerItem
+              icon={({ color, size }) => (
+                <Icon name="settings-outline" color={color} size={size} />
+              )}
+              label="Settings"
+              onPress={() => {}}
+            />
+            <DrawerItem
+              icon={({ color, size }) => (
+                <Icon name="account-check-outline" color={color} size={size} />
+              )}
+              label="About"
+              onPress={() => {
+                props.navigation.navigate("About");
+              }}
+            />
+          </Drawer.Section>
         </View>
-    );
+      </DrawerContentScrollView>
+      <Drawer.Section style={styles.bottomDrawerSection}>
+        <DrawerItem
+          icon={({ color, size }) => (
+            <Icon name="exit-to-app" color={color} size={size} />
+          )}
+          label="Log In"
+          onPress={() => {
+            props.navigation.navigate("Inputs");
+          }}
+        />
+      </Drawer.Section>
+      {/*
+       Keep log out button until we create a new navigator when user is logged
+      in we can swap from log out button to log in. Or make a new drawer navigation
+      for a logged in user and add the log out button.
+      ---*/}
+
+      <Drawer.Section style={styles.bottomDrawerSection}>
+        <DrawerItem
+          icon={({ color, size }) => (
+            <Icon name="exit-to-app" color={color} size={size} />
+          )}
+          label="Log Out"
+          onPress={() => {
+            props.navigation.navigate("Inputs");
+          }}
+        />
+      </Drawer.Section>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    drawerContent: {
-        flex: 1,
-    },
-    userInfoSection: {
-        paddingLeft: 20,
-    },
-    title: {
-        fontSize: 16,
-        marginTop: 3,
-        fontWeight: 'bold',
-    },
-    caption: {
-        fontSize: 14,
-        lineHeight: 14,
-    },
-    row: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginTop: 20,
-    },
-    section: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginRight: 15,
-    },
-    paragraph: {
-        fontWeight: 'bold',
-        marginRight: 3,
-    },
-    drawerSection: {
-        marginTop: 15,
-    },
-    bottomDrawerSection: {
-        marginBottom: 15,
-        borderTopColor: '#f4f4f4',
-        borderTopWidth: 1,
-    },
-    preference: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingVertical: 12,
-        paddingHorizontal: 16,
-    }
+  drawerContent: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingLeft: 20,
+  },
+  title: {
+    fontSize: 16,
+    marginTop: 3,
+    fontWeight: "bold",
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  section: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 15,
+  },
+  paragraph: {
+    fontWeight: "bold",
+    marginRight: 3,
+  },
+  drawerSection: {
+    marginTop: 15,
+  },
+  bottomDrawerSection: {
+    marginBottom: 15,
+    borderTopColor: "#f4f4f4",
+    borderTopWidth: 1,
+  },
+  preference: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
 });
