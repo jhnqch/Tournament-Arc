7eb15304a3e089c987e8622936222de15bbaa4d1
John Quach <quachjohnhien@gmail.com>
2021-02-25 11:33:20 -0800

    Login Page Amplify
    Login Page

--- App.js
+++ App.js
@@ -3,16 +3,6 @@
   This is the main file that the program will run 
 
 ---*/
-<<<<<<< Updated upstream
-import React, { useState } from 'react';
-import * as Font from 'expo-font';
-import Home from './screens/home';
-import {AppLoading} from 'expo';
-import { AppNavigator } from './routes/drawer';
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
-=======
 
 import React, { useState } from "react";
 import * as Font from "expo-font";
@@ -23,7 +13,6 @@ import { Text, View, StyleSheet } from "react-native";
 import Amplify from "aws-amplify";
 import config from "./aws-exports";
 import { Authenticator } from "aws-amplify-react-native";
->>>>>>> Stashed changes
 
 Amplify.configure(config);
 
@@ -37,29 +26,6 @@ export default function App() {
   );
 }
 
-<<<<<<< Updated upstream
-/*--- 
-This comment is a test comment to put while in testFile branch
----*/
-
-export default function App(){
-  const [fontsLoaded, setFontsLoaded] = useState(false)
-  new Account("example@email.com", "user1", "123")
-
-  if(fontsLoaded){
-      return (
-          <AppNavigator />
-      );
-  } else {
-      return (
-          <AppLoading
-              startAsync={getFonts}
-              onFinish={() => setFontsLoaded(true)}
-          />
-      )
-  }
-}
-=======
 const styles = StyleSheet.create({
   containers: {
     flex: 1,
@@ -67,4 +33,3 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
->>>>>>> Stashed changes
